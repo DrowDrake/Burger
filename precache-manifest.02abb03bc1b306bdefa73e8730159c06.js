7b1bb2f07425aab95bc0789d0ffa0b4d536c873d
Petro Loy <drowdrake@gmail.com>
2020-05-07 22:49:34 +0300

    Updates

---
+++ precache-manifest.02abb03bc1b306bdefa73e8730159c06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a8fcf45015729a5569b084bd5b302b",
+    "url": "/Burger/index.html"
+  },
+  {
+    "revision": "fb6d829395f81a492b65",
+    "url": "/Burger/static/css/main.3732b6cd.chunk.css"
+  },
+  {
+    "revision": "b869330819dcdfcf04b1",
+    "url": "/Burger/static/js/2.8b5bbd96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Burger/static/js/2.8b5bbd96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb6d829395f81a492b65",
+    "url": "/Burger/static/js/main.943458c4.chunk.js"
+  },
+  {
+    "revision": "62efe1612cac5cfc0be7",
+    "url": "/Burger/static/js/runtime-main.88d6e803.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
